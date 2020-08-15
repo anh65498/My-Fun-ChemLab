1f19db4b9b78064e87bd3f40d5839280e0ccf907
AlexandraChiritaACS <46438366+AlexandraChiritaACS@users.noreply.github.com>
2020-08-16 01:52:43 +0300

    Update ResultPage.jsx

--- front-end/src/pages/ResultPage.jsx
+++ front-end/src/pages/ResultPage.jsx
@@ -13,15 +13,47 @@ class ResultPage extends React.Component {
   
   constructor() {
     super();
-    this.state = { data: [],
-                  isLoaded: false };
+    this.state = { data: [], load: false};
   }
 
   // @alexandra_chirita added function for database query 
  
+  async componentDidMount() {
+    var getEcho = async () => {
+
+    var result = fetch('https://pg-app-hh3vkhgay7334zu3vh9917fdyhrsyw.scalabl.cloud/1/classes/Chemical_Reactions', {
+      method:'GET',
+      headers: {
+        "X-Parse-Application-Id": "DJONj9M6G3IBwGy3QvgjtOKD8ECR5DTQEKAQRKcz",
+        "X-Parse-Rest-Api-Key": "BoxBHlH6rOD2Q2b3wJHBcLBxzATXN8QREEKpRTLc"
+      }
+    })
+    .then(function(response) {
+        return response.text();
+    })
+    .then(function(data){
+        var obj = null;
+        var data_obj = JSON.parse(data);
+        var count = Object.keys(data_obj.results).length;
+        for (var i = 0; i < count; i++) {
+          if (data_obj.results[i].Reactants == "H2O")
+            {
+              obj = data_obj.results[i];
+              break;
+            }
+        }
+        console.log([obj.EchoAR_Link, obj.EchoAR_Link2, obj.EchoAR_Link3]);
+
+        return [obj.EchoAR_Link, obj.EchoAR_Link2, obj.EchoAR_Link3];
+    })
+  };
+
+  this.state.data = getEcho();
+  this.state.load = true;
+  }
+
   render() {
-    
-  //while (this.state.isLoaded == false);
+    while(this.state.load == false);
     return (
       <Fragment>
         <div id="background">
@@ -59,7 +91,7 @@ class ResultPage extends React.Component {
             </Grid>
           </Grid>
           <Typography>H2O + CO2 = H2CO3</Typography>
-
+          <Typography>{this.state.data}</Typography>
         </div>
       </Fragment>
     );
