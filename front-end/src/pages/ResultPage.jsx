07af014412f475b64970b978627f7d3a8f41997b
AlexandraChiritaACS <46438366+AlexandraChiritaACS@users.noreply.github.com>
2020-08-16 12:27:58 +0300

    Update ResultPage.jsx

--- front-end/src/pages/ResultPage.jsx
+++ front-end/src/pages/ResultPage.jsx
@@ -20,51 +20,38 @@ class ResultPage extends React.Component {
     console.log("Resulting Page");
     console.log(this.props.chemString);
   }
-
-  // @alexandra_chirita added function for database query
-  // async componentDidMount(): Promise<void> {
-  //   try {
-  //     const response = await fetch(
-  //       "https://pg-app-hh3vkhgay7334zu3vh9917fdyhrsyw.scalabl.cloud/1/classes/Chemical_Reactions",
-  //       {
-  //         method: "GET",
-  //         headers: {
-  //           "X-Parse-Application-Id":
-  //             "DJONj9M6G3IBwGy3QvgjtOKD8ECR5DTQEKAQRKcz",
-  //           "X-Parse-Rest-Api-Key": "BoxBHlH6rOD2Q2b3wJHBcLBxzATXN8QREEKpRTLc",
-  //         },
-  //       }
-  //     )
-  //       .then(function (response) {
-  //         return response.text();
-  //       })
-  //       .then(function (data) {
-  //         var obj = null;
-  //         var data_obj = JSON.parse(data);
-  //         var count = Object.keys(data_obj.results).length;
-  //         for (var i = 0; i < count; i++) {
-  //           if (data_obj.results[i].Reactants == "H2O") {
-  //             obj = data_obj.results[i];
-  //             break;
-  //           }
-  //         }
-  //         console.log([obj.EchoAR_Link, obj.EchoAR_Link2, obj.EchoAR_Link3]);
-
-  //         return [obj.EchoAR_Link, obj.EchoAR_Link2, obj.EchoAR_Link3];
-  //       });
-  //     if (!response.ok) {
-  //       throw Error(response.statusText);
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-
-  //   await new Promise((resolve) => {
-  //     setTimeout(resolve, 10000);
-  //   });
-  //   this.state.data = response;
-  //   return Promise.resolve();
-  // }
+  
+  // @alexandra_chirita added fetch method
+   componentDidMount() {
+    let self = this;
+    fetch(
+      "https://pg-app-hh3vkhgay7334zu3vh9917fdyhrsyw.scalabl.cloud/1/classes/Chemical_Reactions",
+      {
+        method: "GET",
+        headers: {
+          "X-Parse-Application-Id": "DJONj9M6G3IBwGy3QvgjtOKD8ECR5DTQEKAQRKcz",
+          "X-Parse-Rest-Api-Key": "BoxBHlH6rOD2Q2b3wJHBcLBxzATXN8QREEKpRTLc",
+        },
+      }
+    )
+      .then(function (response) {
+        return response.text();
+      })
+      .then(function (data) {
+        var obj = null;
+        var data_obj = JSON.parse(data);
+        var count = Object.keys(data_obj.results).length;
+        for (var i = 0; i < count; i++) {
+          if (data_obj.results[i].Reactants === this.props.chemString || data_obj.results[i].Word_Name === this.props.chemString) {
+            obj = data_obj.results[i];
+            break;
+          }
+        }
+        self.setState({
+          data: obj,
+        });
+      });
+  }
 
   render() {
     const { classes } = this.props; // for styling
@@ -74,10 +61,13 @@ class ResultPage extends React.Component {
           <Grid container alignItems="center" className={classes.gridContainer}>
             <Grid item xs={3}>
               <iframe
-                src="https://console.echoar.xyz/webar?key=aged-poetry-0182&entry=f317da3d-5fc8-44c2-8af5-ff686db0c0e4"
+               src={
+                  "https://console.echoar.xyz/webar?key=dry-rain-8136&entry=" +
+                  this.state.data.EchoAR_Link
+                }
                 allow="camera *"
               />
-              <Typography variant="h5">Methane</Typography>
+              <Typography variant="h5">this.state.data.Reactant1</Typography>
             </Grid>
             <Grid item xs={1}>
               <Typography variant="h1" display="inline">
@@ -86,10 +76,13 @@ class ResultPage extends React.Component {
             </Grid>
             <Grid item xs={3}>
               <iframe
-                src="https://console.echoar.xyz/webar?key=aged-poetry-0182&entry=e612c1d3-bc72-49f7-b048-3a41888518ab"
+               src={
+                  "https://console.echoar.xyz/webar?key=aged-poetry-0182&entry=" +
+                  this.state.data.EchoAR_Link2
+                }
                 allow="camera *"
               />
-              <Typography variant="h5">Hydrogen</Typography>
+              <Typography variant="h5">this.state.data.Reactant2</Typography>
             </Grid>
             <Grid item xs={2}>
               <Typography variant="h1" display="inline">
@@ -98,15 +91,18 @@ class ResultPage extends React.Component {
             </Grid>
             <Grid item xs={3}>
               <iframe
-                src="https://console.echoar.xyz/webar?key=aged-poetry-0182&entry=32952071-4354-4fe3-9db1-ef365081d222"
+               src={
+                  "https://console.echoar.xyz/webar?key=aged-poetry-0182&entry=" +
+                  this.state.data.EchoAR_Link3
+                }
                 allow="camera *"
               />
-              <Typography variant="h5">Water</Typography>
+              <Typography variant="h5">this.state.data.Words_Result</Typography>
             </Grid>
           </Grid>
           <Typography variant="h3" className={classes.fullReaction}>
             {/* H2O + CO2 = H2CO3 */}
-            {this.props.chemString}
+            {this.state.data.Words_Name + " -> " + this.state.data.Words_Result}
           </Typography>
         </div>
       </Fragment>
