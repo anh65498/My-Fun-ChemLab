228150aacef85415ab4a89f9d2be7722ea1919a5
AlexandraChiritaACS <46438366+AlexandraChiritaACS@users.noreply.github.com>
2020-08-16 10:27:20 +0300

    Update ResultPage.jsx

--- front-end/src/pages/ResultPage.jsx
+++ front-end/src/pages/ResultPage.jsx
@@ -13,17 +13,18 @@ class ResultPage extends React.Component {
   
   constructor() {
     super();
-    this.state = { data: [], load: false};
+    this.state = { data: []};
   }
 
   // @alexandra_chirita added function for database query 
- 
+
+
   async componentDidMount() {
 
 
-    var getEcho = new Promise(async () => {
+    return new Promise(async () => {
 
-    var result = fetch('https://pg-app-hh3vkhgay7334zu3vh9917fdyhrsyw.scalabl.cloud/1/classes/Chemical_Reactions', {
+    this.state.data = fetch('https://pg-app-hh3vkhgay7334zu3vh9917fdyhrsyw.scalabl.cloud/1/classes/Chemical_Reactions', {
       method:'GET',
       headers: {
         "X-Parse-Application-Id": "DJONj9M6G3IBwGy3QvgjtOKD8ECR5DTQEKAQRKcz",
@@ -38,32 +39,29 @@ class ResultPage extends React.Component {
         var data_obj = JSON.parse(data);
         var count = Object.keys(data_obj.results).length;
         for (var i = 0; i < count; i++) {
-          if (data_obj.results[i].Reactants == "H2O")
+          if (data_obj.results[i].Reactants == "CH4")
             {
               obj = data_obj.results[i];
               break;
             }
         }
-        console.log([obj.EchoAR_Link, obj.EchoAR_Link2, obj.EchoAR_Link3]);
 
+        console.log([obj.EchoAR_Link, obj.EchoAR_Link2, obj.EchoAR_Link3]);
         return [obj.EchoAR_Link, obj.EchoAR_Link2, obj.EchoAR_Link3];
     })
   });
-
-  this.state.data = await getEcho;
-  this.state.load = true;
-  console.log("Result" + this.state.data);
   }
 
   render() {
-  
-    return (
+  console.log("Result");
+    console.log(this.state.data);
+      return (
       <Fragment>
         <div id="background">
           <Grid container alignItems="center">
             <Grid item xs={3}>
               <iframe
-                src="https://console.echoar.xyz/webar?key=aged-poetry-0182&entry=f317da3d-5fc8-44c2-8af5-ff686db0c0e4"
+                src={"https://console.echoar.xyz/webar?key=aged-poetry-0182&entry="}
                 allow="camera *"
               />
               <Typography>Methane</Typography>
@@ -75,7 +73,7 @@ class ResultPage extends React.Component {
             </Grid>
             <Grid item xs={3}>
               <iframe
-                src="https://console.echoar.xyz/webar?key=aged-poetry-0182&entry=e612c1d3-bc72-49f7-b048-3a41888518ab"
+                src={"https://console.echoar.xyz/webar?key=aged-poetry-0182&entry="}
                 allow="camera *"
               />
               <Typography>Hydrogen</Typography>
@@ -87,7 +85,7 @@ class ResultPage extends React.Component {
             </Grid>
             <Grid item xs={3}>
               <iframe
-                src="https://console.echoar.xyz/webar?key=aged-poetry-0182&entry=32952071-4354-4fe3-9db1-ef365081d222"
+                src={"https://console.echoar.xyz/webar?key=aged-poetry-0182&entry="}
                 allow="camera *"
               />
               <Typography>Water</Typography>
